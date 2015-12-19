0c98c08d1f2537b1966ecd5d3f6c8ab1205ef8f9
ravi karre <rkarre1506@gmail.com>
2015-12-18 16:05:35 -0800

    My first commit

---
+++ src/test/java/karre/pranathi/LoginTest.java
@@ -0,0 +1,24 @@
+package karre.pranathi;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void setup()
+	{
+		driver = new FirefoxDriver();
+	}
+	
+	@Test
+	public void dologin()
+	{
+		driver.get("http://gmail.com");
+	}
+	
+}
